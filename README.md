278bd14eb55b8f1f597bfde865feea0e3c9126f0
Olivia Park <olivia.park1346@gmail.com>
2020-07-06 20:33:20 +0900

    first commit
    Added script.js

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # treehouse-project-5
+This project is creating an app for a fictional company called the Awesome Startup. The app uses a Random User Generator API (https://randomuser.me/).
---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+  }
+});
+
+function searchMarkup() {
+
+}
+
+function galleryMarkup() {
+
+}
+
+function modalMarkup() {
+
+}
