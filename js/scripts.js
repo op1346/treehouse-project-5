ab23bc4da5bcc1aea54dbabb860fd949c187f641
Olivia Park <olivia.park1346@gmail.com>
2020-07-27 16:05:39 +0900

    added JQuery and galleryMarkup function

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,65 @@
-fetch('https://randomuser.me/api/?results=12')
+//fetching data from API
+fetch("https://randomuser.me/api/?results=12")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => galleryMarkup(data.results))
+  .catch(error => {
+    console.log(error);
+  })
 
-//user data constants
-const image = user.picture;
-const name = user.name;
-const email = user.email;
-const location = user.location;
+//using data here to append the gallery markup to the DOM
+function galleryMarkup(users) {
+  const gallery = document.getElementById("gallery");
+  const cards = [];
 
-function userDirectory() {
+  users.map(user => {
+    const userCard =
+    `<div class="card">
+      <div class="card-img-container">
+          <img class="card-img" src="${user.picture.large}" alt="profile picture">
+      </div>
+      <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+          <p class="card-text">${user.email}</p>
+          <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+      </div>
+    </div>`;
+    cards.push(userCard);
+  });
+
+  cards.forEach(card => {
+    gallery.innerHTML += card;
+  });
+
+  const userCardButton = document.querySelectorAll(".card");
+  userCardButton.map(card => {
+    card.addEventListener('click', (e) => {
+      if (e.target === card || card.contains(e.target)) {
+          modalMarkup(cards)
+      }
+  });
+  });
 
 }
 
-
-function galleryMarkup() {
-
+//adding the card view
+function modalMarkup(users, index) {
+  const user = users[index];
+  const modalContainer = document.createElement("div");
+  const modal =
+    `
+    <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+        <p class="modal-text">${user.email}</p>
+        <p class="modal-text cap">${user.location.city}</p>
+        <hr>
+        <p class="modal-text">(555) 555-5555</p>
+        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+        <p class="modal-text">Birthday: 10/21/2015</p>
+      </div>
+    </div>`;
+  modalContainer.setAttribute("class", "modal-container");
 }
 
-function modalMarkup() {
-
-}
